eeb4a579e5e830e00cc620c434e80fd73f9c2e3d
pa1myself <kumarpavam869.com>
2021-10-06 15:04:09 +0530

    first commit

---
+++ dist/main.js
@@ -0,0 +1,42 @@
+// 'use strict';
+
+// var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+// function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// class User{
+//   register(){
+//     console.log('User Registered...');
+//   }
+// }
+
+// let test = 'hello';
+
+// class User{
+//   blocked(){
+//     console.log('User blocked...');
+//   }
+//}
+
+
+// var User = function () {
+//   function User(username, email, password) {
+//     _classCallCheck(this, User);
+
+//     this.username = username;
+//     this.email = email;
+//     this.password = password;
+//   }
+
+//   _createClass(User, [{
+//     key: 'register',
+//     value: function register() {
+//       console.log(this.username + 'is now registered');
+//     }
+//   }]);
+
+//   return User;
+// }();
+
+// var bob = new User('bob', 'bob@email.com', '123');
+// bob.register();
---
+++ src/main.js
@@ -0,0 +1,52 @@
+// class User{
+//   register(){
+//     console.log('User Registered...');
+//   }
+// }
+
+// let test = 'hello';
+
+// class User{
+//   blocked(){
+//     console.log('User blocked...');
+//   }
+//}
+
+
+// class User{
+//   constructor(username, email, password){
+//     this.username = username;
+//     this.email = email;
+//     this.password = password;
+//   }
+//   register(){
+//     console.log(this.username+'is now registered');
+//   }
+// }
+
+// let bob = new User('bob', 'bob@email.com', '123');
+// bob.register();
+
+
+// var a = 'Test1';
+// let b = 'Test2';
+
+
+// function testVar(){
+//   var a = 30;
+//   if(true){
+//     var a = 50;
+//     console.log(a);
+//   }
+//   console.log(a);
+// }
+
+
+// function testLet(){
+//   let a = 30;
+//   if(true){
+//     let a = 50;
+//     console.log(a);
+//   }
+//   console.log(a);
+// }
